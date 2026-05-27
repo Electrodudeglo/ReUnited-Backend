a5499defd722e5379f22c867fd1ec62d5d4ed0cf
Mohamed <mohamedmohamed1812@gmail.com>
2026-05-27 15:52:20 +0100

    Add: Re-add Microsoft.EntityFrameworkCore.SqlServer. For some reason, the package was missing from the latest pull. Add: Add test method GetAllItems_Returns_Ok_With_List_Of_Items

---
+++ ReUnited-Test/Controllers-test/LostItemsController-Test.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Moq;
+using ReUnited_Backend.Controllers;
+using ReUnited_Backend.DataModels;
+using ReUnited_Backend.Services;
+
+namespace ReUnited_Test;
+
+public class LostItemsController_Test
+{
+
+    private LostItemsController _lostItemController;
+    private Mock<ILostItemService> _lostItemsServiceMoq;
+   
+    [SetUp]
+    public void Setup()
+    {
+        _lostItemsServiceMoq = new Mock<ILostItemService>();
+        _lostItemController = new LostItemsController(_lostItemsServiceMoq.Object);
+     
+    }
+
+    [Test]
+    public void GetAllItems_Returns_Ok_With_List_Of_Items()
+    {
+
+        List<LostItem> lostItems = new List<LostItem>
+        {
+            new LostItem(),
+            new LostItem()
+        };
+        
+        _lostItemsServiceMoq.Setup(s => s.GetAllItems()).Returns(lostItems);
+
+        OkObjectResult result = _lostItemController.GetAllItems() as OkObjectResult;
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(200, result.StatusCode);
+        Assert.AreEqual(lostItems, result.Value);
+
+    }
+}
