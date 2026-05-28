da065df736a51e5422f71e8054e07506bb208397
Mobolaji Otunuga <mobolaji.otunuga@hotmail.com>
2026-05-28 12:34:11 +0100

    update: arguments in constructor

--- ReUnited-Backend/DataModels/LostItem.cs
+++ ReUnited-Backend/DataModels/LostItem.cs
@@ -3,14 +3,24 @@
     using System.ComponentModel.DataAnnotations;
     public class LostItem
     {
-        public LostItem()
+        public LostItem(
+            string city,
+            string postcode,
+            string email,
+            string phoneNumber,
+            string category,
+            string itemDescription,
+            string additionalInformation,
+            string picture)
         {
-            City = string.Empty;    
-            Postcode = string.Empty;
-            Category = string.Empty;    
-            ItemDescription = string.Empty;
-            Picture = string.Empty;
-            
+            City = city;
+            Postcode = postcode;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Category = category;
+            ItemDescription = itemDescription;
+            AdditionalInformation = additionalInformation;
+            Picture = picture;
         }
         [Key]
         public int Id { get; set; }
@@ -39,6 +49,7 @@
         [MaxLength(500)]
         public string ItemDescription { get; set; } = string.Empty;
 
+        [Required]
         public string AdditionalInformation { get; set; } = string.Empty;
 
         [Required]
