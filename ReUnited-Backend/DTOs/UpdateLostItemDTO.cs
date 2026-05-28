a7ba3194cffb9cc7e2723c818326821ffa6adc0e
Imad Ahmed <j166a775433@gmail.com>
2026-05-28 16:35:14 +0100

    Add lost item DTO

---
+++ ReUnited-Backend/DTOs/UpdateLostItemDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReUnited_Backend.DTOs
+{
+    public class UpdateLostItemDTO
+    {
+        public string City { get; set; } 
+
+        public string Postcode { get; set; } 
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string Category { get; set; } 
+
+        public string ItemDescription { get; set; } 
+
+        public string AdditionalInformation { get; set; } 
+
+        public string Picture { get; set; } 
+
+    }
+}
