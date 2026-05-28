386abcf754e1e1b632ba4bddc0daa0296328d725
Imad Ahmed <j166a775433@gmail.com>
2026-05-28 11:46:14 +0100

    Add methods to utils

--- ReUnited-Backend/Utils.cs
+++ ReUnited-Backend/Utils.cs
@@ -1,6 +1,32 @@
-﻿namespace ReUnited_Backend
+﻿using System.Text.Json;
+
+namespace ReUnited_Backend
 {
     public class Utils
     {
+        public static List<T> GetFileContent<T>(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<T>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+
+        public static string ReSerialize<T>(List<T> input)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            return JsonSerializer.Serialize(input, options);
+        }
+
+        public static void WriteFileContent(string filePath, string jsonFile)
+        {
+            File.WriteAllText(filePath, jsonFile);
+        }
+
     }
 }
