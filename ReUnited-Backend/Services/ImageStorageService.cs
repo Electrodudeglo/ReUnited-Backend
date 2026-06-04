e703a1c6c362ddef1873e6a92e449a6430b03d4c
Imad Ahmed <j166a775433@gmail.com>
2026-06-04 13:59:57 +0100

    Add fields and constructor

--- ReUnited-Backend/Services/ImageStorageService.cs
+++ ReUnited-Backend/Services/ImageStorageService.cs
@@ -4,5 +4,13 @@ namespace ReUnited_Backend.Services
 {
     public class ImageStorageService
     {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public ImageStorageService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
     }
 }
