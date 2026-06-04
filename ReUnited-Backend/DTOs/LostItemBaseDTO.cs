287b455810bec4a5dc4404386b458a4dfd49b146
Imad Ahmed <j166a775433@gmail.com>
2026-06-04 14:50:41 +0100

    Add base DTO class

--- ReUnited-Backend/DTOs/LostItemBaseDTO.cs
+++ ReUnited-Backend/DTOs/LostItemBaseDTO.cs
@@ -1,6 +1,34 @@
-﻿namespace ReUnited_Backend.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReUnited_Backend.DTOs
 {
     public class LostItemBaseDTO
     {
+        [Required]
+        [MaxLength(100)]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public string Postcode { get; set; } = string.Empty;
+
+        [EmailAddress]
+        [MaxLength(255)]
+        public string? Email { get; set; }
+
+        [Phone]
+        [StringLength(11, MinimumLength = 11)]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Category { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(500)]
+        public string ItemDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string AdditionalInformation { get; set; } = string.Empty;
     }
 }
