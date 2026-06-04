408392cd8cac00a70a246155256e57f7d0f8d2a3
Imad Ahmed <j166a775433@gmail.com>
2026-06-04 13:56:11 +0100

    Add Supabase settings properties

--- ReUnited-Backend/Services/SupabaseSettings.cs
+++ ReUnited-Backend/Services/SupabaseSettings.cs
@@ -2,5 +2,10 @@
 {
     public class SupabaseSettings
     {
+        public string Url { get; set; } = string.Empty;
+
+        public string Bucket { get; set; } = string.Empty;
+
+        public string ApiKey { get; set; } = string.Empty;
     }
 }
