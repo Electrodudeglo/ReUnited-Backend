cca7062da4c32793be7a206463a00143f053c9f5
Imad Ahmed <j166a775433@gmail.com>
2026-06-05 14:36:54 +0100

    Add LostItemResponseDTO

---
+++ ReUnited-Backend/DTOs/LostItemResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace ReUnited_Backend.DTOs
+{
+    public class LostItemResponseDTO : LostItemBaseDTO
+    {
+        public int Id { get; set; }
+        public string Picture { get; set; } = string.Empty;
+    }
+}
